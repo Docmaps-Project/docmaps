7e34ec907399a25680184b261661fdfef20b9264
early evening <eve@eatbigger.fish>
2023-02-15 16:32:10 -0800

    Add another test example

--- scripts/package-tests.js
+++ scripts/package-tests.js
@@ -2,7 +2,8 @@ import path from 'path'
 import {fileURLToPath} from 'url'
 import { validateShacl } from './validate-shacl.js'
 
-// TODO rewrite with Jest
+// TODO rewrite with Jest -- currently, because it runs in streams
+//    the pass/fail does not connect with which test example failed in stdoot connect with which test example failed in stdout
 
 const __filename = path.basename(import.meta.url);
 const __dirname = path.dirname(import.meta.url);
@@ -12,6 +13,7 @@ const ROOT_DIR = path.resolve(fileURLToPath(__dirname), "..")
 const TEST_EXAMPLES = [
   // TODO : generate these .NT files from JSON-LD as part of testing
   `${ROOT_DIR}/examples/docmaps-example-embo-01.jsonld.nt`,
+  `${ROOT_DIR}/examples/docmaps-example-elife-01.jsonld.nt`,
 ]
 
 const SHACL_FILE = `${ROOT_DIR}/docmaps-shapes.ttl`
