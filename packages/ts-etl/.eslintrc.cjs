e5e9ae377f423c1ce1b335b41e82a91f6fb37259
early evening <eve@eatbigger.fish>
2023-03-29 10:25:54 -0700

    add eslint config and prettier config

---
+++ packages/ts-etl/.eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  root: true,
+  ignorePatterns: ['dist/'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
+}
