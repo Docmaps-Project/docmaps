c03f8bb443b485ca35cba54ba5ee1e6d48f7c061
early evening <eve@eatbigger.fish>
2023-05-18 11:48:34 -0700

    add another elife example

--- packages/ts-sdk/src/test/types.test.ts
+++ packages/ts-sdk/src/test/types.test.ts
@@ -26,70 +26,70 @@ test('Codec parsing OnlineAccount', (t) => {
   const v = ex.elife.OnlineAccount.flatMap((x) => {
     return dm.OnlineAccount.decode(x)
   })
-  isRightArray(t, v, 1)
+  isRightArray(t, v, 2)
 })
 
 test('Codec parsing Publisher', (t) => {
   const v = ex.elife.Publisher.flatMap((x) => {
     return dm.Publisher.decode(x)
   })
-  isRightArray(t, v, 2)
+  isRightArray(t, v, 3)
 })
 
 test('Codec parsing Manifestation', (t) => {
   const v = ex.elife.Manifestation.flatMap((x) => {
     return dm.Manifestation.decode(x)
   })
-  isRightArray(t, v, 21)
+  isRightArray(t, v, 33)
 })
 
 test('Codec parsing Thing', (t) => {
   const v = ex.elife.Thing.flatMap((x) => {
     return dm.Thing.decode(x)
   })
-  isRightArray(t, v, 9)
+  isRightArray(t, v, 15)
 })
 
 test('Codec parsing RoleInTime', (t) => {
   const v = ex.elife.RoleInTime.flatMap((x) => {
     return dm.RoleInTime.decode(x)
   })
-  isRightArray(t, v, 9)
+  isRightArray(t, v, 13)
 })
 
 test('Codec parsing Actor', (t) => {
   const v = ex.elife.Actor.flatMap((x) => {
     return dm.Actor.decode(x)
   })
-  isRightArray(t, v, 9)
+  isRightArray(t, v, 13)
 })
 
 test('Codec parsing Action', (t) => {
   const v = ex.elife.Action.flatMap((x) => {
     return dm.Action.decode(x)
   })
-  isRightArray(t, v, 9)
+  isRightArray(t, v, 15)
 })
 
 test('Codec parsing Assertion', (t) => {
   const v = ex.elife.Assertion.flatMap((x) => {
     return dm.Assertion.decode(x)
   })
-  isRightArray(t, v, 6)
+  isRightArray(t, v, 10)
 })
 
 test('Codec parsing Step', (t) => {
   const v = ex.elife.Step.flatMap((x) => {
     return dm.Step.decode(x)
   })
-  isRightArray(t, v, 5)
+  isRightArray(t, v, 8)
 })
 
 test('Codec parsing Docmap', (t) => {
   const v = ex.elife.Docmap.flatMap((x) => {
     return dm.Docmap.decode(x)
   })
-  isRightArray(t, v, 2, (arr) => {
+  isRightArray(t, v, 3, (arr) => {
     t.deepEqual(arr[0]?.['@context'], 'https://w3id.org/docmaps/context.jsonld')
   })
 })
@@ -99,7 +99,7 @@ test('Codec inserts missing @context key for Docmap', (t) => {
     const { ['@context']: _, ...stripped } = x
     return dm.Docmap.decode(stripped)
   })
-  isRightArray(t, v, 2, (arr) => {
+  isRightArray(t, v, 3, (arr) => {
     t.deepEqual(arr[0]?.['@context'], 'https://w3id.org/docmaps/context.jsonld')
   })
 })
