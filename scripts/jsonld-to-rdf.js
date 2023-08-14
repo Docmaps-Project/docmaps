132e3ca7b00479cb8dd780d134a6923659205711
early evening <eve@eatbigger.fish>
2023-08-14 09:55:52 -0600

    refactor to improve rdf handling script

--- scripts/jsonld-to-rdf.js
+++ scripts/jsonld-to-rdf.js
@@ -1,41 +1,69 @@
-import ParserJsonld from '@rdfjs/parser-jsonld'
-import Serializer from '@rdfjs/serializer-turtle'
-import ParserN3 from '@rdfjs/parser-n3'
-import formats from '@rdfjs/formats-common'
-import SerializerNtriples from '@rdfjs/serializer-ntriples'
-import { Readable, Transform } from 'stream'
+import ParserJsonld from "@rdfjs/parser-jsonld";
+import Serializer from "@rdfjs/serializer-turtle";
+import ParserN3 from "@rdfjs/parser-n3";
+import formats from "@rdfjs/formats-common";
+import SerializerNtriples from "@rdfjs/serializer-ntriples";
+import { Readable, Transform } from "stream";
 import fs from "fs";
+import readline from "readline";
 
-const BASE_IRI='https://w3id.org/docmaps/examples/'
+const BASE_IRI = "https://w3id.org/docmaps/examples/";
 
-async function loadFile(fileName) {
+async function processFile(fileName, callback) {
   try {
-    const data = await fs.promises.readFile(fileName, "utf-8");
-    return data;
+    const readStream = readline.createInterface({
+      input: fs.createReadStream(fileName, "utf-8"),
+    });
+
+    let i = 0;
+
+    for await (const line of readStream) {
+      console.error(
+        `${i} ... Processing graph: ${line.substring(0, 120)} with length ${line.length
+        }`,
+      );
+      await callback(line);
+      i++;
+    }
   } catch (err) {
     console.error(err);
   }
 }
 
+async function processGraphString(g) {
+  const input = new Readable({
+    read: () => {
+      input.push(g);
+      input.push(null);
+    },
+  });
+
+  return new Promise((res, rej) => {
+    const parserJsonld = new ParserJsonld({
+      baseIRI: BASE_IRI,
+      steamingProfile: false,
+    });
+    const serializerNtriples = new SerializerNtriples();
+
+    // TODO: include declaration that a docmap is a :Docmap
+    const quads = parserJsonld.import(input);
+
+    const nt = serializerNtriples.import(quads);
+    nt.on("data", (d) => {
+      process.stdout.write(d);
+    })
+      .on("end", () => {
+        res();
+      })
+      .on("close", (err) => {
+        if (err) {
+          rej(err);
+        } else {
+          res();
+        }
+      });
+  });
+}
+
 const fileName = process.argv[2];
-const  data = await loadFile(fileName);
-
-const parserJsonld = new ParserJsonld({baseIRI: BASE_IRI})
-const serializerNtriples = new SerializerNtriples()
-const serializer = new Serializer()
-
-const input = new Readable({
-  read: () => {
-    input.push(data)
-    input.push(null)
-  }
-})
-
-// TODO: include declaration that a docmap is a :Docmap
-const quads = parserJsonld.import(input)
-
-const nt = serializerNtriples.import(quads)
-
-// TODO: add conversion to turtle as well.
-nt.pipe(process.stdout)
-
+const _ = await processFile(fileName, processGraphString);
