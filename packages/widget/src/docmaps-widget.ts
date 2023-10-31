e9750c3c8bb8b628471780fb658c955e584e7992
Andrew Edstrom <andrewedstrom@gmail.com>
2023-10-30 20:57:52 -0600

    click the button to add a node!

--- packages/widget/src/docmaps-widget.ts
+++ packages/widget/src/docmaps-widget.ts
@@ -3,24 +3,22 @@ import { customElement, property } from 'lit/decorators.js';
 import { customCss } from './styles';
 import * as d3 from 'd3';
 
-/**
- * An example element.
- *
- * @slot - This element has a slot
- * @csspart button - The button
- */
+type Node = { x: number; y: number; id: string };
+type LinkDeclaration = { source: string; target: string };
+type RenderedLink = { source: Node; target: Node };
+
 @customElement('docmaps-widget')
 export class DocmapsWidget extends LitElement {
   @property({ type: Number })
   count = 3;
 
-  nodes = [
+  nodes: Node[] = [
     { id: 'A', x: 100, y: 150 },
     { id: 'B', x: 300, y: 150 },
     { id: 'C', x: 200, y: 300 },
   ];
 
-  links = [
+  links: LinkDeclaration[] = [
     { source: 'A', target: 'B' },
     { source: 'B', target: 'C' },
     { source: 'C', target: 'A' },
@@ -32,12 +30,15 @@ export class DocmapsWidget extends LitElement {
     this.count++;
 
     // Create a new node with a unique ID
-    const newNode = { id: 'N' + this.count, x: Math.random() * 500, y: Math.random() * 300 };
-    this.nodes.push(newNode);
-    console.log(this.nodes)
+    const newNode = { id: 'N' + this.count, x: Math.random() * 215, y: Math.random() * 175 };
+
     // Connect the new node to a random existing node
     const existingNode = this.nodes[Math.floor(Math.random() * this.nodes.length)];
     this.links.push({ source: newNode.id, target: existingNode.id });
+
+    // Add the node after the link so the node isn't linked to itself
+    this.nodes.push(newNode);
+
     // Re-render the graph
     this.drawGraph();
   }
@@ -51,14 +52,17 @@ export class DocmapsWidget extends LitElement {
       }
       const svg = d3.select(canvas).append('svg').attr('width', 500).attr('height', 300);
 
-      const displayNodes = JSON.parse(JSON.stringify(this.nodes));
-      const displayLinks = JSON.parse(JSON.stringify(this.links));
+      const displayNodes: Node[] = JSON.parse(JSON.stringify(this.nodes));
+      const displayLinks: RenderedLink[] = JSON.parse(JSON.stringify(this.links));
       // Initialize force layout
       const simulation = d3
         .forceSimulation(displayNodes)
         .force(
           'link',
-          d3.forceLink(displayLinks).id((d) => d.id),
+          d3.forceLink(displayLinks).id((d) => {
+            // @ts-ignore
+            return d.id;
+          }),
         )
         .force('charge', d3.forceManyBody())
         .force('collide', d3.forceCollide(30)) // 25 is the radius for collision detection
@@ -87,6 +91,18 @@ export class DocmapsWidget extends LitElement {
         .attr('fill', 'green')
         .attr('r', 20);
 
+      const labels = svg
+        .append('g')
+        .attr('class', 'labels')
+        .selectAll('text')
+        .data(displayNodes)
+        .enter()
+        .append('text')
+        .attr('text-anchor', 'middle')
+        .attr('dy', '.35em') // Vertically center
+        .attr('fill', 'white')
+        .text((d) => d.id);
+
       // Update positions on each simulation tick
       simulation.on('tick', () => {
         link
@@ -96,6 +112,7 @@ export class DocmapsWidget extends LitElement {
           .attr('y2', (d) => d.target.y);
 
         node.attr('cx', (d) => d.x).attr('cy', (d) => d.y);
+        labels.attr('x', (d) => d.x).attr('y', (d) => d.y);
       });
     }
   }
