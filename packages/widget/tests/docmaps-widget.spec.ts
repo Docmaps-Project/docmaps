4bcb27d1d00337bc1e3d4f4b8e247d93f2c0753c
Andrew Edstrom <andrewedstrom@gmail.com>
2023-10-31 11:43:02 -0600

    add playwright tests

---
+++ packages/widget/tests/docmaps-widget.spec.ts
@@ -0,0 +1,20 @@
+import { expect, Page, test } from '@playwright/test'
+
+
+test('clicking button increments the count', async ({ page }) => {
+  await page.goto('/')
+
+  await expectNodeCount(page, 3)
+  await expect(page.getByRole('button', { name: 'Add 4th Node' })).toBeVisible()
+
+  await page.getByRole('button', { name: 'Add 4th Node' }).click()
+
+  await expect(page.getByRole('button', { name: 'Add 5th Node' })).toBeVisible()
+  await expectNodeCount(page, 4)
+})
+
+async function expectNodeCount(page: Page, n: number) {
+  const d3Canvas = await page.waitForSelector('#d3-canvas')
+  const circles = await d3Canvas.$$('circle')
+  expect(circles).toHaveLength(n)
+}
