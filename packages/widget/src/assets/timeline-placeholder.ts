f43a19d5a6644c9efa88bdd1937979fd5c697625
Andrew Edstrom <andrewedstrom@gmail.com>
2023-11-14 14:45:15 -0700

    no play button; back button instead

--- packages/widget/src/assets/timeline-placeholder.ts
+++ packages/widget/src/assets/timeline-placeholder.ts
@@ -1,17 +1,19 @@
 import { svg } from 'lit';
 
-const playButton = svg`
+const backButton = svg`
   <svg width='36' height='36' viewBox='0 0 36 36' fill='none' xmlns='http://www.w3.org/2000/svg'>
     <circle cx='17.5' cy='17.5' r='17' stroke='#474747'/>
-    <path d='M25.5 17.134C26.1667 17.5189 26.1667 18.4811 25.5 18.866L13.5 25.7942C12.8333 26.1791 12 25.698 12 24.9282L12 11.0718C12 10.302 12.8333 9.82087 13.5 10.2058L25.5 17.134Z' fill='#474747'/>
-  </svg>`;
+    <path transform="scale(-1,1) translate(-35,0)" d='M22.5 17.134C23.1667 17.5189 23.1667 18.4811 22.5 18.866L10.5 25.7942C9.83333 26.1791 9 25.698 9 24.9282L9 11.0718C9 10.302 9.83333 9.82087 10.5 10.2058L22.5 17.134Z' fill='#474747'/>
+    <path transform="scale(-1,1) translate(-35,0)"  d='M24 10L24 26' stroke='#474747' stroke-width='2' stroke-linecap='round'/>
+<!--    <circle cx='24' cy='18' r='3' fill='#474747'/>-->
+  </svg>`
 
 const forwardButton = svg`
   <svg width='36' height='36' viewBox='0 0 36 36' fill='none' xmlns='http://www.w3.org/2000/svg'>
     <circle cx='17.5' cy='17.5' r='17' stroke='#474747'/>
     <path d='M22.5 17.134C23.1667 17.5189 23.1667 18.4811 22.5 18.866L10.5 25.7942C9.83333 26.1791 9 25.698 9 24.9282L9 11.0718C9 10.302 9.83333 9.82087 10.5 10.2058L22.5 17.134Z' fill='#474747'/>
     <path d='M24 10L24 26' stroke='#474747' stroke-width='2' stroke-linecap='round'/>
-    <circle cx='24' cy='18' r='3' fill='#474747'/>
+<!--    <circle cx='24' cy='18' r='3' fill='#474747'/>-->
   </svg>`;
 
 const timeline = svg`
@@ -28,6 +30,6 @@ const timeline = svg`
   </svg>`;
 
 export const timelinePlaceholder = svg`
-  ${playButton}
+  ${backButton}
   ${forwardButton}
   ${timeline}`;
