5be50aa743ec596657b7cdae9b332048f3cb8623
Andrew Edstrom <andrewedstrom@gmail.com>
2023-11-20 19:52:45 -0700

    autoformat and change copy

--- packages/spa/src/App.svelte
+++ packages/spa/src/App.svelte
@@ -1,7 +1,6 @@
 <script>
-  import { onMount } from 'svelte';
   import { configureForDoiString, structureError } from './utils.js';
-  import JsonBox from "./JsonBox.svelte";
+  import JsonBox from './JsonBox.svelte';
   import '@docmaps/widget';
 
   let inputDoi = '';
@@ -14,22 +13,27 @@
   function handleData(data) {
     let config = {
       display: {
-	publisherName: name => name || "Preprint posted on Crossref"
-      }
-    }
+        publisherName: name => name || 'Preprint posted on Crossref',
+      },
+    };
 
-    json=data;
+    json = data;
   }
 
   function handleError(error) {
-    json=structureError(error);
+    json = structureError(error);
   }
 
   async function fetchData() {
     if (!widgetElement) {
+      const widgetExplanation = document.createElement('b');
+      widgetExplanation.appendChild(document.createTextNode('Docmaps Widget (data fetched from docmaps staging server)'));
+      placeholder.appendChild(widgetExplanation);
+      placeholder.appendChild(document.createElement('br'));
+
       widgetElement = document.createElement('docmaps-widget');
-      widgetElement.setAttribute('serverurl', 'https://web-nodejs.onrender.com')
-      widgetElement.setAttribute('doi', inputDoi)
+      widgetElement.setAttribute('serverurl', 'https://web-nodejs.onrender.com');
+      widgetElement.setAttribute('doi', inputDoi);
       placeholder.appendChild(widgetElement);
     }
     await configureForDoiString(
@@ -42,51 +46,47 @@
 
 <main>
   <h1>Demo: crossref-to-docmap</h1>
-  <input
-    type="text"
-    bind:value="{inputDoi}"
-    placeholder="Enter your DOI here"
-  />
+  <input type='text' bind:value='{inputDoi}' placeholder='Enter your DOI here' />
   <!-- Your other markup and code -->
-  <button on:click="{fetchData}">Fetch Data</button>
-  <div id="result" bind:this="{placeholder}"></div>
-  <div class="code-container">
-    <b>Derived Docmap contents:</b>
-    <JsonBox {json} bind:this="{codeBox}"/>
+  <button on:click='{fetchData}'>Fetch Data</button>
+  <div id='result' bind:this='{placeholder}'></div>
+  <div class='code-container'>
+    <b>Raw docmap (derived from crossref):</b>
+    <JsonBox {json} bind:this='{codeBox}' />
   </div>
 </main>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  #result {
-    padding-top: 3em;
-    padding-bottom: 3em;
-    margin: auto;
-    max-width: 500px;
-  }
-
-  .code-container {
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  input {
-    min-width: 400px;
-  }
-
-  @media (min-width: 640px) {
     main {
-      max-width: none;
+        text-align: center;
+        padding: 1em;
+        margin: 0 auto;
     }
-  }
+
+    #result {
+        padding-top: 3em;
+        padding-bottom: 3em;
+        margin: auto;
+        max-width: 500px;
+    }
+
+    .code-container {
+    }
+
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+
+    input {
+        min-width: 400px;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
