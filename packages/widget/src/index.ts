0a65c1558bfbd767e0ae9bd915a4b434f2c67623
Andrew Edstrom <andrewedstrom@gmail.com>
2023-11-21 14:49:04 -0700

    don't export stuff we don't want to expose externally

--- packages/widget/src/index.ts
+++ packages/widget/src/index.ts
@@ -1,9 +1,3 @@
 // All files which will be accessible when the widget is installed via npm are declared here
 export * from './docmaps-widget';
 export * from './docmap-controller';
-export { emptyMetadataMessage } from './detail-view.ts';
-export { createGridItem } from './detail-view.ts';
-export { createMetadataGrid } from './detail-view.ts';
-export { renderDetailsView } from './detail-view.ts';
-export { addLinkToDocumentHeader } from './font.ts';
-export { loadFont } from './font.ts';
