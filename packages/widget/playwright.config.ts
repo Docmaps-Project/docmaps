18a416578b785437647113d2812da2e6ff49e039
Andrew Edstrom <andrewedstrom@gmail.com>
2023-11-21 13:27:02 -0700

    try running in all browsers. why not

--- packages/widget/playwright.config.ts
+++ packages/widget/playwright.config.ts
@@ -29,24 +29,24 @@ const all_browsers = [
   },
 
   /* Test against mobile viewports. */
-  // {
-  //   name: 'Mobile Chrome',
-  //   use: { ...devices['Pixel 5'] },
-  // },
-  // {
-  //   name: 'Mobile Safari',
-  //   use: { ...devices['iPhone 12'] },
-  // },
+  {
+    name: 'Mobile Chrome',
+    use: { ...devices['Pixel 5'] },
+  },
+  {
+    name: 'Mobile Safari',
+    use: { ...devices['iPhone 12'] },
+  },
 
   /* Test against branded browsers. */
-  // {
-  //   name: 'Microsoft Edge',
-  //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-  // },
-  // {
-  //   name: 'Google Chrome',
-  //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-  // },
+  {
+    name: 'Microsoft Edge',
+    use: { ...devices['Desktop Edge'], channel: 'msedge' },
+  },
+  {
+    name: 'Google Chrome',
+    use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+  },
 ];
 
 /**
@@ -61,16 +61,27 @@ const url: string = 'http://localhost:5173';
  */
 export default defineConfig({
   testDir: './test/integration',
+  /* The base directory, relative to the config file, for snapshot files created with toMatchSnapshot and toHaveScreenshot. */
+  snapshotDir: './__snapshots__',
+
+  /* Maximum time one test can run for. */
+  timeout: 30 * 1000,
+
   /* Run tests in files in parallel */
   fullyParallel: true,
+
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: IS_CI,
+
   /* Retry on CI only */
   retries: IS_CI ? 2 : 0,
+
   /* Opt out of parallel tests on CI. */
   workers: IS_CI ? 1 : undefined,
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: IS_CI ? [['list'], ['html']] : 'list',
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -80,10 +91,10 @@ export default defineConfig({
     trace: 'on-first-retry',
   },
 
-  /* Configure projects for major browsers */
+  /* Which browsers to run the tests in */
   projects: IS_CI || ALL_BROWSERS ? all_browsers : localBrowsers,
 
-  /* Run your local dev server before starting the tests */
+  /* Run local dev server before starting the tests */
   webServer: {
     command: 'pnpm run dev',
     url,
