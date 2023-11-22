75c477f4220bb04ccb7d96c44f2d2dffb5d881ac
eve n.u. <eve@eatbigger.fish>
2023-11-22 13:59:35 -0800

    merge same display objects
    - also, refactor some test stuff
    - improve type handling
    - rename methods
    - generate nodeIds nondeterministically

--- packages/widget/test/unit/docmap-controller.test.ts
+++ packages/widget/test/unit/docmap-controller.test.ts
@@ -1,10 +1,6 @@
 import test from 'ava';
-import {
-  getSteps,
-  sortDisplayObjectEdges,
-  sortDisplayObjects,
-  stepsToGraph,
-} from '../../src';
+import { getSteps, stepsToGraph } from '../../src';
+import type { DisplayObjectEdge } from '../../src/util';
 import smallDocmapWithOneStep from '../fixtures/sciety-docmap-1';
 import largeDocmapWithOneStep from '../fixtures/sciety-docmap-2';
 import docmapWithMultipleSteps from '../fixtures/elife-docmap-1';
@@ -18,7 +14,7 @@ test('getSteps when there is only one step', (t) => {
   const steps = getSteps(smallDocmapWithOneStep);
   t.is(steps.length, 1);
   const step = steps[0];
-  t.is(step.inputs[0].doi, '10.21203/rs.3.rs-1043992/v1');
+  t.is(step.inputs?.[0].doi, '10.21203/rs.3.rs-1043992/v1');
   t.is(step.actions[0].outputs[0].type, 'review-article');
 });
 
@@ -29,10 +25,7 @@ test('getSteps when there are multiple steps', (t) => {
   const originalPreprintPublication = steps[0].assertions[0];
   t.is(originalPreprintPublication.status, 'manuscript-published');
   t.is((originalPreprintPublication.item as Item).type, 'preprint');
-  t.is(
-    (originalPreprintPublication.item as Item).doi,
-    '10.1101/2022.11.08.515698',
-  );
+  t.is((originalPreprintPublication.item as Item).doi, '10.1101/2022.11.08.515698');
 
   t.is(steps[1].assertions.length, 2);
 
@@ -44,29 +37,20 @@ test('getSteps when there are multiple steps', (t) => {
   const originalPreprintWithNewStatus = steps[1].assertions[1];
   t.is(originalPreprintWithNewStatus.status, 'under-review');
   t.is((originalPreprintWithNewStatus.item as Item).type, 'preprint');
-  t.is(
-    (originalPreprintWithNewStatus.item as Item).doi,
-    '10.1101/2022.11.08.515698',
-  );
+  t.is((originalPreprintWithNewStatus.item as Item).doi, '10.1101/2022.11.08.515698');
 
   t.is(steps[2].assertions.length, 1);
   const originalPreprintPeerReviewed = steps[2].assertions[0];
   t.is(originalPreprintPeerReviewed.status, 'peer-reviewed');
   t.is((originalPreprintPeerReviewed.item as Item).type, 'preprint');
-  t.is(
-    (originalPreprintPeerReviewed.item as Item).doi,
-    '10.1101/2022.11.08.515698',
-  );
+  t.is((originalPreprintPeerReviewed.item as Item).doi, '10.1101/2022.11.08.515698');
 });
 
 test('getSteps on non-docmaps', (t) => {
   const notADocmap = { this: 'is', some: 'randomly', shaped: ['object'] };
   const err = t.throws(() => getSteps(notADocmap), { instanceOf: TypeError });
 
-  t.is(
-    err?.message,
-    'Could not parse Docmap: {"this":"is","some":"randomly","shaped":["object"]}',
-  );
+  t.is(err?.message, 'Could not parse Docmap: {"this":"is","some":"randomly","shaped":["object"]}');
 });
 
 test('getSteps on docmaps without a first step', (t) => {
@@ -85,22 +69,20 @@ test('stepsToGraph for a docmap with one step', (t) => {
 
   t.is(nodes.length, 2);
 
-  const sortedNodes = sortDisplayObjects(nodes);
-  t.like(sortedNodes[0], {
+  t.like(nodes[0], {
     nodeId: '10.21203/rs.3.rs-1043992/v1',
     doi: '10.21203/rs.3.rs-1043992/v1',
     type: '??',
   });
 
-  t.like(sortedNodes[1], {
-    nodeId: 'n1',
+  t.like(nodes[1], {
     type: 'review-article',
   });
 
   t.deepEqual(edges, [
     {
       sourceId: '10.21203/rs.3.rs-1043992/v1',
-      targetId: 'n1',
+      targetId: nodes[1].nodeId,
     },
   ]);
 });
@@ -111,37 +93,33 @@ test('stepsToGraph for a larger docmap with one step', (t) => {
 
   t.is(nodes.length, 4);
 
-  const sortedNodes = sortDisplayObjects(nodes);
-  t.like(sortedNodes[0], {
+  t.like(nodes[0], {
     doi: '10.21203/rs.3.rs-3171736/v1',
     nodeId: '10.21203/rs.3.rs-3171736/v1',
     type: '??',
   });
-  t.like(sortedNodes[1], {
-    nodeId: 'n1',
+  t.like(nodes[1], {
     type: 'review-article',
   });
-  t.like(sortedNodes[2], {
-    nodeId: 'n2',
+  t.like(nodes[2], {
     type: 'review-article',
   });
-  t.like(sortedNodes[3], {
-    nodeId: 'n3',
+  t.like(nodes[3], {
     type: 'review-article',
   });
 
   t.deepEqual(edges, [
     {
       sourceId: '10.21203/rs.3.rs-3171736/v1',
-      targetId: 'n1',
+      targetId: nodes[1].nodeId,
     },
     {
       sourceId: '10.21203/rs.3.rs-3171736/v1',
-      targetId: 'n2',
+      targetId: nodes[2].nodeId,
     },
     {
       sourceId: '10.21203/rs.3.rs-3171736/v1',
-      targetId: 'n3',
+      targetId: nodes[3].nodeId,
     },
   ]);
 });
@@ -152,37 +130,42 @@ test('stepsToGraph for a docmap with multiple steps', (t) => {
 
   t.is(nodes.length, 6);
 
-  const sortedNodes = sortDisplayObjects(nodes);
-  t.like(sortedNodes[0], {
-    doi: '10.1101/2022.11.08.515698',
-    nodeId: '10.1101/2022.11.08.515698',
-    type: 'preprint',
-  });
+  [
+    {
+      doi: '10.1101/2022.11.08.515698',
+      nodeId: '10.1101/2022.11.08.515698',
+      type: 'preprint',
+    },
 
-  t.like(sortedNodes[1], {
-    doi: '10.7554/eLife.85111.1',
-    nodeId: '10.7554/eLife.85111.1',
-    type: 'preprint',
-  });
-  t.like(sortedNodes[2], {
-    doi: '10.7554/eLife.85111.1.sa1',
-    nodeId: '10.7554/eLife.85111.1.sa1',
-    type: 'reply',
-  });
-  t.like(sortedNodes[3], {
-    doi: '10.7554/eLife.85111.1.sa2',
-    nodeId: '10.7554/eLife.85111.1.sa2',
-    type: 'review-article',
-  });
-  t.like(sortedNodes[4], {
-    doi: '10.7554/eLife.85111.1.sa3',
-    nodeId: '10.7554/eLife.85111.1.sa3',
-    type: 'review-article',
-  });
-  t.like(sortedNodes[5], {
-    doi: '10.7554/eLife.85111.1.sa4',
-    nodeId: '10.7554/eLife.85111.1.sa4',
-    type: 'evaluation-summary',
+    {
+      doi: '10.7554/eLife.85111.1',
+      nodeId: '10.7554/eLife.85111.1',
+      type: 'preprint',
+    },
+    {
+      doi: '10.7554/eLife.85111.1.sa1',
+      nodeId: '10.7554/eLife.85111.1.sa1',
+      type: 'reply',
+    },
+    {
+      doi: '10.7554/eLife.85111.1.sa2',
+      nodeId: '10.7554/eLife.85111.1.sa2',
+      type: 'review-article',
+    },
+    {
+      doi: '10.7554/eLife.85111.1.sa3',
+      nodeId: '10.7554/eLife.85111.1.sa3',
+      type: 'review-article',
+    },
+    {
+      doi: '10.7554/eLife.85111.1.sa4',
+      nodeId: '10.7554/eLife.85111.1.sa4',
+      type: 'evaluation-summary',
+    },
+  ].forEach((expectedNode) => {
+    const matches = nodes.filter((n) => n.nodeId === expectedNode.nodeId);
+    t.is(matches.length, 1);
+    t.like(matches[0], expectedNode);
   });
 
   const sortedEdges = sortDisplayObjectEdges(edges);
@@ -209,3 +192,9 @@ test('stepsToGraph for a docmap with multiple steps', (t) => {
     },
   ]);
 });
+
+function sortDisplayObjectEdges(edges: DisplayObjectEdge[]): DisplayObjectEdge[] {
+  return [...edges].sort((a: DisplayObjectEdge, b: DisplayObjectEdge) =>
+    a.sourceId.localeCompare(b.sourceId),
+  );
+}
